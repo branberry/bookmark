b1d158cb72cd45756b19a7eae2eb2682b9ce9220
Brandon Ly <brandonly@lostcoding.com>
2022-01-23 16:14:08 -0600

    rename project to recall

--- src/service/background.ts
+++ src/service/background.ts
@@ -1,7 +1,22 @@
+const CONTEXT_MENU_ID = 'BOOKMARK_CONTEXT_ID'
+
+function createContextMenu() {
+  chrome.contextMenus.create({
+    id: CONTEXT_MENU_ID,
+    title: 'Search bookmark',
+    contexts: ['selection', 'page'],
+  })
+
+  chrome.contextMenus.onClicked.addListener((info) => {
+    console.log(JSON.stringify(info, null, 2))
+  })
+}
+
 function main() {
   chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ hello: 'world!' })
     console.log('set!!')
+    createContextMenu()
   })
 }
 
